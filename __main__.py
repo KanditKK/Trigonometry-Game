2354d00e5402e123203cc5a4e2725f8e710e2832
KanditKK <kandit.kor@gmail.com>
2022-09-06 17:12:44 +0700

    Major Changes C220906-1-J
    Added 3 Enemy Types and Models
    Added 2 Enemy Bullet Models
    Added Enemy and Player hitbox
    Change Background Moving Speed
    Added Enemy renderer
    Added new player model
    Added Despawn mechanism
    Added Game reset mechanism
    Added player bullet auto-shoot
    Added Enemy Spawning mechanism
    Added 'create bullet with angle'
    Added score add mechanism

--- __main__.py
+++ __main__.py
@@ -19,23 +19,38 @@ while True:
         resources.events = pygame.event.get() #get events for calculate wasd and quit actions.
         util.checkQuit() #check if quit
 
-        #enemy.bullet.create(random.randint(0, screen.panelWidth - 10), 0, player.posx, player.posy)
+        if pygame.time.get_ticks() % 100 and len(enemy.list) < 2 and enemy.spawnCooldown <= 0: enemy.spawn(random.randint(1,3)); enemy.spawnCooldown = 25
 
         enemy.bullet.cycle() #add bullet (x, y) by angle(sin, cos).
         player.bullet.createCycle() #add bullet if spacebar is pressed.
         player.bullet.cycle() #add bullet y by 5 px.
+
+        enemy.cycle()
+        enemy.despawn()
+        #enemy.bullet.hitbox()
+        enemy.bullet.despawn()
+
         player.cycle() #add player (x, y) change by wasd and calculate boundaries detection.
+        player.bullet.hitbox()
+        player.bullet.despawn()
+
+        if enemy.bullet.hitbox(): score.stage = 3
 
         screen.renderBackground() #calculate and blit background to screen.
         screen.renderEnemyBullet() #blit all enemy's bullets to screen.
         screen.renderPlayerBullet() #blit all player's bullets to screen
         screen.renderPlayer() #blit player with (x, y) to screen.
+        screen.renderEnemy()
         screen.update() #update pygame screen.
 
-        score.add(1) #add score by calculate game ticks.
+        if pygame.time.get_ticks() % 100 == 0: score.add(1) #add score by calculate game ticks.
         score.checkStage() #check stage by score.
 
-        print([len(enemy.bullet.list), player.bullet.isShooting , player.bullet.list ])
+        print([score.Score, len(player.bullet.list), len(enemy.bullet.list)])
         clock.tick(50)
 
     score.reset()
+    player.reset()
+    enemy.reset()
+    enemy.bullet.reset()
+
--- game_module.py
+++ game_module.py
@@ -42,8 +42,8 @@ class screen:
         pygame.display.update()
 
     def renderBackground():
-        screen.backgroundY1 += 5
-        screen.backgroundY2 += 5
+        screen.backgroundY1 += 3
+        screen.backgroundY2 += 3
         if screen.backgroundY1 >= screen.panelHeight: screen.backgroundY1 = -(screen.panelHeight * 3)
         if screen.backgroundY2 >= screen.panelHeight: screen.backgroundY2 = -(screen.panelHeight * 3)
         screen.gameDisplay.blit(resources.image.background, (0, screen.backgroundY1))
@@ -54,20 +54,26 @@ class screen:
 
     def renderEnemyBullet():
         for data in enemy.bullet.list:
-            screen.gameDisplay.blit(resources.image.enemyBullet, (data[0] + screen.panelxOfset + 7, data[1] + screen.panelyOfset + 7))
+            screen.gameDisplay.blit(resources.image.enemyBullet[data[3]-1], (data[0] + screen.panelxOfset + 7, data[1] + screen.panelyOfset + 7))
 
     def renderPlayerBullet():
         for data in player.bullet.list:
             screen.gameDisplay.blit(resources.image.playerBullet, (data[0] + screen.panelxOfset, data[1] + screen.panelyOfset))
     def renderPlayer():
         screen.gameDisplay.blit(resources.image.player, (player.posx + screen.panelxOfset, player.posy + screen.panelyOfset))
-
-
+    def renderEnemy():
+        for data in enemy.list:
+            if data[2] == 1:
+                screen.gameDisplay.blit(resources.image.enemyType1, (data[0] + screen.panelxOfset, data[1] + screen.panelyOfset))
+            if data[2] == 2:
+                screen.gameDisplay.blit(resources.image.enemyType2, (data[0] + screen.panelxOfset, data[1] + screen.panelyOfset))
+            if data[2] == 3:
+                screen.gameDisplay.blit(resources.image.enemyType3, (data[0] + screen.panelxOfset, data[1] + screen.panelyOfset))
 class player:
     posx = screen.panelWidth // 2
     posy = (screen.panelHeight // 2) + 100
     width = 100
-    height = 108
+    height = 100
     xpc = 0
     xmc = 0
     ypc = 0
@@ -88,7 +94,33 @@ class player:
             for i in range(len(player.bullet.list)):
                 player.bullet.list[i][1] -= 15
 
+        def hitbox():
+            for b in range(len(player.bullet.list)):
+                for e in range(len(enemy.list)):
+                    bulletX = player.bullet.list[b][0]
+                    bulletY = player.bullet.list[b][1]
+                    enemyX = enemy.list[e][0]
+                    enemyY = enemy.list[e][1]
 
+                    if bulletX >= enemyX and bulletX <= enemyX+enemy.width and bulletY >= enemyY and bulletY <= enemyY+enemy.height:
+                        player.bullet.list[b][1] = -player.bullet.height-10
+                        enemy.list[e][1] = screen.displayHeight
+                        score.add(2)
+
+        def despawn():
+            newls = []
+            for i in range(len(player.bullet.list)):
+                if player.bullet.list[i][1] > 0-player.bullet.height: newls.append(player.bullet.list[i])
+            player.bullet.list.clear()
+            for data in newls:
+                player.bullet.list.append(data)
+    def reset():
+        player.posx = screen.panelWidth // 2
+        player.posy = (screen.panelHeight // 2) + 100
+        player.xpc = 0
+        player.xmc = 0
+        player.ypc = 0
+        player.ymc = 0
     def cycle():
         for event in resources.events:
             if event.type == pygame.KEYDOWN:
@@ -102,7 +134,8 @@ class player:
                     player.ypc = 10
                 if event.key == pygame.K_SPACE:
                     player.bullet.isShooting = True
-
+                if event.key == pygame.K_g:
+                    player.bullet.isShooting = not player.bullet.isShooting
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_a:
                     player.xmc = 0
@@ -123,33 +156,107 @@ class player:
         if player.posx <= 0: player.posx = 0
         if player.posy <= 0: player.posy = 0
 class enemy:
-    list = []  # [x, y, type]
+    list = []  # [x, y, type, nextXpath, bulletcooldown, shootcooldown]
+    width = 50
+    height = 50
+    spawnCooldown = 25
+    #enemy type
+    #1 : target locked
+    #2 : randomly shoot
+
+    def spawn(type):
+        spawnX = (random.randint(0,screen.panelWidth-enemy.width)//2)*2
+        spawnY = -enemy.height
+        nextXpath = (random.randint(0,(screen.panelWidth-enemy.width)//2))*2
+        enemy.list.append([spawnX, spawnY, type, nextXpath, 50, 30])
+    def cycle():
+        enemy.spawnCooldown -= 1
+        for i in range(len(enemy.list)):
+            enemy.list[i][1] += 2
+            enemy.list[i][4] -= 1
+            if enemy.list[i][0] < enemy.list[i][3]: enemy.list[i][0] += 2
+            elif enemy.list[i][0] > enemy.list[i][3]: enemy.list[i][0] -= 2
+            elif enemy.list[i][0] == enemy.list[i][3]: enemy.list[i][3] = (random.randint(0,(screen.panelWidth-enemy.width)//2))*2
+            if enemy.list[i][4] <= 0 :
+                if enemy.list[i][2] == 1 and enemy.list[i][5] % 10 == 0:
+                    enemy.bullet.create(enemy.list[i][0], enemy.list[i][1], player.posx, player.posy, enemy.list[i][2])
+                if enemy.list[i][2] == 2:
+                    for n in range(6):
+                        enemy.bullet.createWithAngle(enemy.list[i][0]+enemy.width//2, enemy.list[i][1]+enemy.height//2, n*60, enemy.list[i][2])
+                    enemy.list[i][5] = 0
+                if enemy.list[i][2] == 3:
+                    enemy.bullet.createWithAngle(enemy.list[i][0]+enemy.width//2, enemy.list[i][1]+enemy.height//2, 180, enemy.list[i][2])
+                    enemy.list[i][4] = 50
+                enemy.list[i][5] -= 1
+            if enemy.list[i][5] < 0:
+                enemy.list[i][4] = 100
+                enemy.list[i][5] = 30
+
+    def despawn():
+        newls = []
+        for i in range(len(enemy.list)):
+            if enemy.list[i][1] < screen.panelHeight: newls.append(enemy.list[i])
+        enemy.list.clear()
+        for data in newls:
+            enemy.list.append(data)
+
+    def reset():
+        enemy.list = []
+        enemy.spawnCooldown = 25
+
     class bullet:
-        list = []  # [x, y, angle_in_degrees]
+        list = []  # [x, y, angle_in_degrees, type]
+        width = 15
+        height = 15
         speed = 5
 
-        def create(enemyX, enemyY, playerX, playerY):
+        def create(enemyX, enemyY, playerX, playerY, type):
             deltaX = enemyX - (playerX + (player.width // 2))
             deltaY = enemyY - (playerY + (player.height // 2))
             angle = int(math.degrees(math.atan2(deltaY, deltaX))) - 180
-            enemy.bullet.list.append([enemyX, enemyY, angle])
+            enemy.bullet.list.append([enemyX, enemyY, angle, type])
+
+        def createWithAngle(enemyX, enemyY, angle, type):
+            enemy.bullet.list.append([enemyX, enemyY, angle, type])
 
         def cycle():
             for i in range(len(enemy.bullet.list)):
-                angle = enemy.bullet.list[i][2]
-                enemy.bullet.list[i][0] += enemy.bullet.speed * math.cos(math.radians(angle))
-                enemy.bullet.list[i][1] += enemy.bullet.speed * math.sin(math.radians(angle))
+                if enemy.bullet.list[i][3] == 3:
+                    enemy.bullet.list[i][1] += 5
+                else:
+                    angle = enemy.bullet.list[i][2]
+                    enemy.bullet.list[i][0] += enemy.bullet.speed * math.cos(math.radians(angle))
+                    enemy.bullet.list[i][1] += enemy.bullet.speed * math.sin(math.radians(angle))
+        def hitbox():
+            isHit = False
+            for b in range(len(enemy.bullet.list)):
+                bulletX = enemy.bullet.list[b][0]
+                bulletY = enemy.bullet.list[b][1]
+                playerX = player.posx
+                playerY = player.posy
 
+                if bulletX >= playerX and bulletX <= playerX+player.width and bulletY >= playerY and bulletY <= playerY+player.height:
+                    enemy.bullet.list[b][1] = -enemy.bullet.height-10
+                    isHit = True
+            return isHit
+        def despawn():
+            newls = []
+            for i in range(len(enemy.bullet.list)):
+                if enemy.bullet.list[i][1] < screen.panelHeight and enemy.bullet.list[i][1] > -enemy.bullet.height: newls.append(enemy.bullet.list[i])
+            enemy.bullet.list.clear()
+            for data in newls:
+                enemy.bullet.list.append(data)
+        def reset(): enemy.bullet.list = []
 class score:
     Score = 0
     highScore = 0
     heart = 3
     stage = 1
     def add(type):
-        if type == 0:
-            score.Score += 1
         if type == 1:
-            pass
+            score.Score += 1
+        if type == 2:
+            score.Score += 10
     def removeHeart():
         score.heart -= 1
 
@@ -174,10 +281,13 @@ class score:
 
 class resources:
     class image:
-        enemyBullet = pygame.image.load('resource/enmBullet1.PNG')
+        enemyBullet = [pygame.image.load('resource/enmBullet1.PNG'), pygame.image.load('resource/enmBullet2.png'), pygame.image.load('resource/enmBullet2.png')]
         playerBullet = pygame.image.load('resource/plyBullet1.png')
-        player = pygame.image.load('resource/battleplane.png')
+        player = pygame.image.load('resource/player.png')
         background = pygame.image.load('resource/background.png')
+        enemyType1 = pygame.image.load('resource/enemy1.png')
+        enemyType2 = pygame.image.load('resource/enemy2.png')
+        enemyType3 = pygame.image.load('resource/enemy3.png')
 
     class sound:
         pass
